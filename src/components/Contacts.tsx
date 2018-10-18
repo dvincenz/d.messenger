a2d6c4fc16427e1f780d7c41ec230529e3e050d1
Dumeni Vincenz <dvincenz@outlook.com>
2018-10-18 22:31:45 +0200

    remove unused dependencies

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { StyleRulesCallback, withStyles, Avatar, ListItem, ListItemText, List } from '@material-ui/core';
-import { Address } from 'cluster';
 
 
 const styles: StyleRulesCallback = theme => ({ 

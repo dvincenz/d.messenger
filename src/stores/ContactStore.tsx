f20dc360039b32da47034c4f1e1065d452357f86
Alessandro <abonomo@hsr.ch>
2018-10-30 14:24:27 +0100

    added ContactStore logic

--- src/stores/ContactStore.tsx
+++ src/stores/ContactStore.tsx
@@ -1,25 +1,91 @@
-import { observable } from "mobx";
+import {flow, observable} from "mobx";
 import { Contact } from "../entities";
+import {Iota} from "../services/iotaService";
+import {settingStore} from "./SettingStore";
+import {IBaseMessage, IContactRequest, IContactResponse, MessageMethod, Permission} from "../entities/interfaces";
+
 
 export class ContactStore {
     @observable public contacts: Contact[] = [];
+    @observable public state: ContactStoreState;
+
+    public fetchContacts = flow(function *(this: ContactStore, address: string) {
+        this.state = ContactStoreState.loading
+        try {
+            const messages = yield this.Iota.getMessages(address)
+            messages.forEach((message: IBaseMessage) => {
+                this.filterMessage(message)
+            })
+            this.state = ContactStoreState.updated
+        } catch (error) {
+            this.state = ContactStoreState.error
+            console.log(error)
+        }
+    })
+
+    private checkAcceptedStateOfContactRequest = flow(function *(this: ContactStore, contact: Contact) {
+        try {
+            const messages = yield this.Iota.getMessages(contact.address)
+            messages.forEach((message: IBaseMessage) => {
+                if(message.method === MessageMethod.ContactResponse) {
+                    const msg = (message as IContactResponse)
+                    if(msg.level === Permission.accepted) {
+                        contact.state = true
+                    }
+                }
+            })
+        } catch (error) {
+            this.state = ContactStoreState.error
+            console.log(error)
+        }
+    })
+
+
+    private Iota: Iota;
+
+    constructor () {
+        this.Iota = new Iota(settingStore.host + ':' + settingStore.port, settingStore.seed, 'IAXUZ9CFIZOIMMQGFUEMYEGPLFYDLBQWYKPMRAGZREMWSGSP9IJUSKBYOLK9DUCVXUDUCBNRPYDUQYLG9IZYKIX9Q9');
+    }
+
 
     public addContact (contact: Contact) {
         this.contacts.push(contact)
+        if(!contact.state) {
+            this.checkAcceptedStateOfContactRequest(contact)
+        }
     }
 
-    public getRequests() {
-        const requests: Contact[] = []
-        this.contacts.forEach(contact => {
-            if(!contact.state) {
-                requests.push(contact)
+    private filterMessage(message: IBaseMessage) {
+        switch (message.method) {
+            case MessageMethod.ContactRequest: {
+                const msg = (message as IContactRequest)
+                const contact = {name: msg.name, address: msg.address, state: false}
+                this.addContact(contact)
+                break
             }
-        })
-        return requests
+            case MessageMethod.ContactResponse: {
+                const msg = (message as IContactResponse)
+                if(msg.level) {
+                    const contact = {name: msg.name, address: msg.address, state: true}
+                    this.addContact(contact)
+                }
+                break
+            }
+        }
     }
 }
 
- 
+
+export const contactStore = new ContactStore();
+
+export enum ContactStoreState{
+    loading,
+    doPoW,
+    updated,
+    error,
+}
+
+
 
 // todo put following logic in contact store
 // private async checkAcceptedStateOfContactRequests(contact: Contact, activeAddr: string) {
@@ -37,6 +103,3 @@ export class ContactStore {
 //         }
 //     })
 // }
-
-
-export const contactStore = new ContactStore();
